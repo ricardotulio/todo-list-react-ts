7595c36baff65ececa6eebb9328d6bad925941e8
Ricardo Tulio <ledo.tulio@gmail.com>
2018-06-06 17:56:16 +0100

    setup: add and configure react

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.ts',
+	devtool: 'source-map',
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: 'awesome-typescript-loader',
         exclude: /node_modules/
-      }
-    ]
+      },
+      {
+  			enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader"
+			},
+    ],
   },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
@@ -18,5 +24,9 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
 }
