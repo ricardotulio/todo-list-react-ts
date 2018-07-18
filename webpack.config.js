ebcf7cdc835b8df560d0ba019ee39cf25cbc0d37
ricardotulio <ledo.tulio@gmail.com>
2018-07-18 22:15:15 +0100

    chore: fix webpack config identation

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-	devtool: 'source-map',
+  devtool: 'source-map',
   entry: './src/index.tsx',
   module: {
     rules: [
@@ -12,10 +12,10 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-  			enforce: "pre",
-				test: /\.js$/,
-				loader: "source-map-loader"
-			},
+  	enforce: "pre",
+	test: /\.js$/,
+	loader: "source-map-loader"
+      },
     ],
   },
   resolve: {
