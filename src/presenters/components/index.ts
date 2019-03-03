765f56650bec96c3b72ebe9e808fd1b3cf905afb
Ricardo <ledo.tulio@gmail.com>
2019-03-03 14:43:06 +0000

    presenters: create module that exports all components

---
+++ src/presenters/components/index.ts
@@ -0,0 +1,12 @@
+import Task from "./Task"
+import TaskStatus from "./TaskStatus"
+import TaskForm from "./TaskForm"
+import TaskFormData from "./TaskForm"
+import TaskList from './TaskList'
+
+export {
+  Task,
+  TaskStatus,
+  TaskForm,
+  TaskList,
+}
