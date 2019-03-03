94e68394161dbc6c28c993d6066540ce630559a3
Ricardo <ledo.tulio@gmail.com>
2019-03-03 14:38:25 +0000

    chore: fix code style into src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 import { App } from "./infraestructure/App";
 
 /* tslint:disable */
 ReactDOM.render(
-    <App />,
-    document.getElementById("app"),
-);
+  <App />,
+  document.getElementById('app'),
+)
 /* tslint:enable */
