c103d85dc49d663a81c5f8e65002c7e3eda4534c
Ricardo <ledo.tulio@gmail.com>
2019-03-03 14:57:32 +0000

    chore: fix code style

--- src/presenters/components/index.ts
+++ src/presenters/components/index.ts
@@ -1,7 +1,7 @@
-import Task from "./Task"
-import TaskStatus from "./TaskStatus"
-import TaskForm from "./TaskForm"
-import TaskFormData from "./TaskForm"
+import Task from './Task'
+import TaskStatus from './TaskStatus'
+import TaskForm from './TaskForm'
+import TaskFormData from './TaskForm'
 import TaskList from './TaskList'
 
 export {
