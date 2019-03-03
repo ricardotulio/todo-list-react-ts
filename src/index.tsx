f8e23d24ea67184513819a205e0a01bc8e917ae7
Ricardo <ledo.tulio@gmail.com>
2019-03-03 14:46:18 +0000

    presenters: create App container
    Create App container that represents an App, that is, its represent
    the root node in the component tree.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { App } from "./infraestructure/App";
+import { App } from './presenters/containers'
 
 /* tslint:disable */
 ReactDOM.render(
---
+++ src/presenters/containers/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import TaskForm from './TaskForm'
+import TaskList from './TaskList'
+
+const App = () => (
+  <div>
+    <TaskList />
+    <TaskForm />
+  </div>
+)
+
+export default App
