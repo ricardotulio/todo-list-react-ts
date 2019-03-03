6e11c3c2494a861bf322ce686b134b5b8244819f
Ricardo <ledo.tulio@gmail.com>
2019-03-03 14:37:01 +0000

    domain: permit findTask to return null if task doesnt exists

--- src/domain/store/index.ts
+++ src/domain/store/index.ts
@@ -1,7 +1,7 @@
 import { Task } from "../entities";
 
 interface TaskStoreInterface {
-  findTask(id: string): Task
+  findTask(id: string): Task|null
   persistTask(task: Task): Task
   removeTask(task: Task): boolean
 }
