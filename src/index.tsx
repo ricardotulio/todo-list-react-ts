c605893acf2dd8f765cf01d08f7a2359263073d9
Ricardo <ledo.tulio@gmail.com>
2019-03-04 12:37:38 +0000

    state: add react-redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './state/task/store'
 
 import { App } from './presenters/containers'
 
 /* tslint:disable */
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app'),
 )
 /* tslint:enable */
