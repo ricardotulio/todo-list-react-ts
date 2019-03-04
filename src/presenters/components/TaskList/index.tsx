567829f1c29e722693ba6b10a762f14c54f0ec57
Ricardo <ledo.tulio@gmail.com>
2019-03-04 12:31:07 +0000

    presenters: remove unused code from TasList component

--- src/presenters/components/TaskList/index.tsx
+++ src/presenters/components/TaskList/index.tsx
@@ -2,11 +2,7 @@ import * as React from 'react'
 import { map } from 'ramda'
 import Task from '../Task'
 
-class Props {
-  taskList: any
-}
-
-const renderTask = (task: any) => (
+const renderTask = (task) => (
   <Task id={task.id}
     title={task.title}
     description={task.description}
@@ -14,7 +10,7 @@ const renderTask = (task: any) => (
   />
 )
 
-const TaskList = (props: Props) => (
+const TaskList = (props) => (
   <div>
     {map(renderTask, props.taskList)}
   </div>
