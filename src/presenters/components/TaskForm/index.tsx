e17ddbc2006a0b47dd77c39efe12afa0b671bb99
Ricardo <ledo.tulio@gmail.com>
2019-03-04 16:37:55 +0000

    presenters: transform 'onSubmit' function in a generic dispatcher

--- src/presenters/components/TaskForm/index.tsx
+++ src/presenters/components/TaskForm/index.tsx
@@ -8,20 +8,21 @@ class FormData {
 
 class Props {
   formData: any
+  onChange: Function
   submitForm: Function
 }
 
-const onSubmit = (submitHandler: Function) => (event: React.FormEvent) => {
+const dispatch = (submitHandler: Function) => (event: React.FormEvent) => {
   event.preventDefault();
   submitHandler(event);
 }
 
 const TaskForm = (props: Props) => (
   <div>
-    <form onSubmit={onSubmit(props.submitForm)}>
-      <input type="hidden" name="id" value={props.formData.id} />
-      <input type="text" name="title" value={props.formData.title} />
-      <textarea name="description" value={props.formData.description}></textarea>
+    <form onSubmit={dispatch(props.submitForm)}>
+      <input type="hidden" name="id" value={props.formData.id} onChange={dispatch(props.onChange)} />
+      <input type="text" name="title" value={props.formData.title} onChange={dispatch(props.onChange)} />
+      <textarea name="description" value={props.formData.description} onChange={dispatch(props.onChange)}></textarea>
       <button type="submit">Save</button>
     </form>
   </div>
