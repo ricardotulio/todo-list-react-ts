7af8440290ef379f4159a03d6ec854f3a5665609
Ricardo <ledo.tulio@gmail.com>
2019-03-04 12:46:30 +0000

    presenters: connect TaskForm container to redux state

--- src/presenters/containers/TaskForm.tsx
+++ src/presenters/containers/TaskForm.tsx
@@ -1,25 +1,24 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
 import { TaskForm as TaskFormComponent } from '../components'
 
-export default class TaskForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      id: '',
-      title: '',
-      description: '',
-    }
-  }
-
+class TaskForm extends React.Component {
   submitForm(event: Event) { }
   
   render() {
+    const { newTask } = this.props
+
     return (
       <TaskFormComponent
-        formData={this.state}
+        formData={newTask}
         submitForm={this.submitForm.bind(this)}
       />
     )
   }
 }
+
+const mapStateToProps = store => ({
+  newTask: store.taskState.newTask,
+})
+
+export default connect(mapStateToProps)(TaskForm)
