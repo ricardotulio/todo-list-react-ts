8f355251000261df40392074f1a6a51cf0bd0c56
Ricardo <ledo.tulio@gmail.com>
2019-03-04 16:39:44 +0000

    presenters: manage TaskForm state using redux

--- src/presenters/containers/TaskForm.tsx
+++ src/presenters/containers/TaskForm.tsx
@@ -1,17 +1,52 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { persistTask } from '../../state/task/actions'
 import { TaskForm as TaskFormComponent } from '../components'
 
 class TaskForm extends React.Component {
-  submitForm(event: Event) { }
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newTask: props.newTask,
+    }
+
+    this.clearForm = this.clearForm.bind(this)
+    this.inputChange = this.inputChange.bind(this)
+  }
+
+  clearForm() {
+    this.setState({
+      newTask: {
+        id: '',
+        title: '',
+        description: '',
+        status: '',
+      }
+    })
+  }
+
+  inputChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+
+    this.setState(state => ({
+      newTask: {
+        ...state.newTask,
+        [name]: value,
+      }
+    }))
+  }
+
   render() {
-    const { newTask } = this.props
+    const { persistTask } = this.props
 
     return (
       <TaskFormComponent
-        formData={newTask}
-        submitForm={this.submitForm.bind(this)}
+        formData={this.state.newTask}
+        onChange={this.inputChange}
+        submitForm={() => persistTask(this.state.newTask) && this.clearForm()}
       />
     )
   }
@@ -21,4 +56,7 @@ const mapStateToProps = store => ({
   newTask: store.taskState.newTask,
 })
 
-export default connect(mapStateToProps)(TaskForm)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ persistTask }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskForm)
