bb1a2353f53855a2cc2b1abef3ed5156de064970
Ricardo <ledo.tulio@gmail.com>
2019-03-10 16:43:28 +0000

    presenters: add key attribute to TaskList item into TaskList component

--- src/presenters/components/TaskList/index.tsx
+++ src/presenters/components/TaskList/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import Task from "../Task"
 
 const renderTask = (task) => (
-  <Task
+  <Task key={task.id}
     id={task.id}
     title={task.title}
     description={task.description}
