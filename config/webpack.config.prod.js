7e60cf24eaaa7a609a32dede26ab5c91be30eed8
Ricardo <ledo.tulio@gmail.com>
2019-03-15 00:00:10 +0000

    setup: change UglifyJsPlugin for webpack optimization

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,15 +1,14 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.config.common.js')
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimize: true,
+  },
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true,
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     })
