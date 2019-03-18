6cc0b170803b4c8fcabc98703f0efe9c480750d5
Ricardo <ledo.tulio@gmail.com>
2019-03-18 15:29:06 +0000

    refactoring: move providers to infraestructure

--- src/infraestructure/providers/CreateTaskControllerProvider.ts
+++ src/infraestructure/providers/CreateTaskControllerProvider.ts
@@ -1,6 +1,6 @@
-import { CreateTaskController } from "../domain/task/controllers"
-import { TaskServiceFactory } from "../domain/task/services"
-import taskStore from "../services/TaskStore"
+import { CreateTaskController } from "../../domain/task/controllers"
+import { TaskServiceFactory } from "../../domain/task/services"
+import taskStore from "../../services/TaskStore"
 
 const CreateTaskControllerProvider = () => ({
   dispatch: CreateTaskController.dispatch(TaskServiceFactory.createTaskService(), taskStore),
